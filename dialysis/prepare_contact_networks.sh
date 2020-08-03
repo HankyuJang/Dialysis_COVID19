93d671d5ec11a301cb91f725aaadbd927cc489a7
Hankyu Jang <hankyu-jang@uiowa.edu>
2020-08-03 17:03:36 -0500

    Modify bash scripts - now HCP to chair distances are computed in these scripts

--- dialysis/prepare_contact_networks.sh
+++ dialysis/prepare_contact_networks.sh
@@ -1,3 +1,4 @@
+python calculate_hcw_chair_distance.py
 python precompute_arrays.py -p data/HCP_locations/
 python precompute_arrays_hcw_chair.py
 python reduce_contacts.py
--- dialysis/prepare_contact_networks_other_days.sh
+++ dialysis/prepare_contact_networks_other_days.sh
@@ -1,19 +1,24 @@
+python calculate_hcw_chair_distance.py -day 2
 python precompute_arrays.py -p data/HCP_locations/ -day 2
 python precompute_arrays_hcw_chair.py -day 2
 python reduce_contacts.py -day 2
 
+python calculate_hcw_chair_distance.py -day 6
 python precompute_arrays.py -p data/HCP_locations/ -day 6
 python precompute_arrays_hcw_chair.py -day 6
 python reduce_contacts.py -day 6
 
+python calculate_hcw_chair_distance.py -day 7
 python precompute_arrays.py -p data/HCP_locations/ -day 7
 python precompute_arrays_hcw_chair.py -day 7
 python reduce_contacts.py -day 7
 
+python calculate_hcw_chair_distance.py -day 8
 python precompute_arrays.py -p data/HCP_locations/ -day 8
 python precompute_arrays_hcw_chair.py -day 8
 python reduce_contacts.py -day 8
 
+python calculate_hcw_chair_distance.py -day 9
 python precompute_arrays.py -p data/HCP_locations/ -day 9
 python precompute_arrays_hcw_chair.py -day 9
 python reduce_contacts.py -day 9
